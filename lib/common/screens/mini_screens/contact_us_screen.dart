13bbbc49bbf220cf1894c6f64d2815da7c11cf93
ben-lawino <lawinwes@gmail.com>
2026-03-28 17:25:41 +0300

    updated help center code

--- lib/common/screens/mini_screens/contact_us_screen.dart
+++ lib/common/screens/mini_screens/contact_us_screen.dart
@@ -12,6 +12,7 @@ class ContactUsScreen extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 18.0),
       child: Column(
         children: [
+          //customer service
           Container(
             width: double.infinity,
             height: 80,
@@ -46,6 +47,8 @@ class ContactUsScreen extends StatelessWidget {
           SizedBox(
             height: 15,
           ),
+
+          //whatsapp
           Container(
             width: double.infinity,
             height: 80,
@@ -69,6 +72,155 @@ class ContactUsScreen extends StatelessWidget {
                 SizedBox(width: 24),
                 Text(
                   'Whatsapp',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontWeight: FontWeight.bold, fontSize: 17),
+                )
+              ],
+            ),
+          ),
+
+          SizedBox(
+            height: 15,
+          ),
+
+          //facebook
+          Container(
+            width: double.infinity,
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 20,
+                  offset: Offset(0, 10),
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 18),
+                Image.asset('assets/icons/facebook.png',
+                    scale: 18, color: primaryColor),
+                SizedBox(width: 24),
+                Text(
+                  'Facebook',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontWeight: FontWeight.bold, fontSize: 17),
+                )
+              ],
+            ),
+          ),
+
+          SizedBox(
+            height: 15,
+          ),
+
+          //instagram
+          Container(
+            width: double.infinity,
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 20,
+                  offset: Offset(0, 10),
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 18),
+                Image.asset('assets/icons/instagram.png',
+                    scale: 18, color: primaryColor),
+                SizedBox(width: 24),
+                Text(
+                  'Instagram',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontWeight: FontWeight.bold, fontSize: 17),
+                )
+              ],
+            ),
+          ),
+
+          SizedBox(
+            height: 15,
+          ),
+
+          //twitter
+          Container(
+            width: double.infinity,
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 20,
+                  offset: Offset(0, 10),
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 18),
+                Image.asset('assets/icons/twitter.png',
+                    scale: 18, color: primaryColor),
+                SizedBox(width: 24),
+                Text(
+                  'Twitter',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontWeight: FontWeight.bold, fontSize: 17),
+                )
+              ],
+            ),
+          ),
+
+
+          SizedBox(
+            height: 15,
+          ),
+
+          //web
+          Container(
+            width: double.infinity,
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 20,
+                  offset: Offset(0, 10),
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 18),
+                Image.asset('assets/icons/web.png',
+                    scale: 18, color: primaryColor),
+                SizedBox(width: 24),
+                Text(
+                  'Website',
                   style: Theme.of(context)
                       .textTheme
                       .titleMedium!
